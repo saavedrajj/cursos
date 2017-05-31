6486f0b6f43a0a8ae72dd08640db78941f1deb42
Saavedra <jusaavedra@paypalcorp.com>
2017-05-31 13:16:49 +0100

    s

---
+++ javascript/anthony.alicea/04.32-F.A.faking.namespaces/app.js
@@ -0,0 +1,14 @@
+var greet = 'Hello!';
+var greet = '¡Hola!';
+
+console.log(greet);
+
+var english = {};
+var spanish = {};
+
+english.greetings{};
+english.greetings.greet = 'Hello!';
+spanish.greet = '¡Hola!';
+
+console.log(spanish);
+console.log(english);
